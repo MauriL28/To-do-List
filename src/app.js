6b0edebb8dceccfcb593128767b657c1eb46f7df
Mauricio Lizcano <mauricio.lizcano@udc.es>
2024-06-16 14:21:51 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+class Task {
+  constructor(description) {
+    this.description = description;
+    this.completed = false;
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  toggleComplete() {
+    this.completed = !this.completed;
+  }
+}
+
+const taskForm = document.getElementById("taskForm");
+const taskList = document.getElementById("taskList");
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const taskDescription = document.getElementById("taskInput").ariaValueMax;
+  const task = new Task(taskDescription);
+  addTaskToDom(task);
+  taskForm.reset();
+});
+
+function addTaskToDom(task) {
+  const li = document.createElement("li");
+  li.textContent = task.description;
+
+  const completeButton = documment.createElement("button");
+  completeButton.textContent = "Complete";
+  completeButton.addEventListener("click", () => {
+    taskList.toggleComplete();
+    li.classList.toggle("completed", task.completed);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+  taskList.appendChild(li);
+}
